f3307ac3b650eea0f73dc6d03f54a7ecacf7c855
EgorBezz <egorbezzubenko1983@gmail.com>
2023-03-26 14:48:08 +0300

    Some new features

--- CostBHKcb.py
+++ CostBHKcb.py
@@ -65,6 +65,7 @@ def result_out(phrase, cost_bdk_out, volume_bdk_out, cost_sk_out, volume_sk_out)
 
 # additional calculations costs and volumes before display it
 
+
 def result(DATA, MODELS_PATH, slab_foundation, first_floor_area, house_mode, floor_seiling, floor_area,
            roof_type, roof_slope_angle, attic_type):
 
@@ -77,24 +78,23 @@ def result(DATA, MODELS_PATH, slab_foundation, first_floor_area, house_mode, flo
     #average price per cube of the rest of the material
     price_rest_vl = 35000
 
-    #average price per cube of finishing floor
-    price_finishing_floor_vl = 48500
+    #average price per cube of finishing floor for baths
+    price_finish_floor_vl = 47000
+
+    #average price per square strapping
+    price_strapping_sq_0 = 900
 
     #average price per square strapping and ground floor for houses (with draft floor)
     price_strapping_sq_1 = 1700
 
     #average price per square strapping and ground floor for baths (with finishing floor)
-    price_strapping_sq_0 = 2855
+    price_strapping_sq_2 = 2855
 
     #price per volume strapping and ground floor for houses (with draft floor)
     price_strapping_vl = 25600
-    price_draft_floor_vl = 25600
 
     #price per square finishing floor
-    price_finish_floor_sq = 2168
-
-    #average price stapping per 1 square of floor area
-    price_strapping_sq = price_strapping_sq_0 - price_finish_floor_sq
+    price_finish_floor_sq = 1955
 
     #calculation cost of basic house kit,
     #volume of wall material,
@@ -107,17 +107,23 @@ def result(DATA, MODELS_PATH, slab_foundation, first_floor_area, house_mode, flo
     volume_bdk_out = round(((cost_bdk_out - cost_sk_out) / price_rest_vl + volume_sk_out), 2)
 
 
-    #some output phrases
+    #comlete any output phrases
+
     phr1 = 'Предварительная стоимость базового домокомплекта: '
     phr2 = 'Предварительная стоимость стенокомплекта: '
-    phr3 = 'Предварительная стоимость домокомплекта на фундаментную плиту'
-    phr4 = 'Предварительная стоимость домокомплекта с '
+    phr3 = 'Предварительная стоимость домокомплекта на фундаментную плиту: '
+    phr4 = 'Предварительная стоимость домокомплекта'
     phr5 = 'Вероятный объём базового домокомплекта: '
     phr6 = 'Вероятный объём стенокомплекта: '
     phr7 = 'Вероятный объём домокомплекта на фундаментную плиту: '
-    phr8 = 'Вероятный объём домокомплекта с '
+    phr8 = 'Вероятный объём домокомплекта'
+    phr9 = ' с чистовым полом и потолком: '
+    phr10 = ' c черновым полом и потолком: '
+    phr11 = ' c чистовым полом и черновым потолком: '
+    phr12 = ' с чистовым потолком и черновым полом: '
+    phr13 = ' на фундаментную плиту: '
 
-    phrase = []
+    phrase = [None] * 4
     phrase[0] = phr1
     phrase[1] = phr5
     phrase[2] = phr2
@@ -127,18 +133,18 @@ def result(DATA, MODELS_PATH, slab_foundation, first_floor_area, house_mode, flo
     #cost and volume of basic house kit for slab foundation
 
     if slab_foundation:
+        phrase[0] = phr3
+        phrase[1] = phr7
         if house_mode:
-            cost_bdk_out = round (cost_bdk_out - first_floor_area * price_strapping_sq_1)
+            cost_bdk_out = round(cost_bdk_out - first_floor_area * price_strapping_sq_1)
             volume_bdk_out = round((volume_bdk_out - (first_floor_area * price_strapping_sq_1) / price_strapping_vl), 2)
         else:
-            cost_bdk_out = round (cost_bdk_out - first_floor_area * price_strapping_sq_0)
-            volume_strapping_floor = (price_strapping_sq / price_strapping_vl) + (price_finish_floor_sq / price_finishing_floor_vl)
+            cost_bdk_out = round (cost_bdk_out - first_floor_area * price_strapping_sq_2)
+            volume_strapping_floor = ((price_strapping_sq_0 / price_strapping_vl) + (price_finish_floor_sq / price_finish_floor_vl)) * first_floor_area
             volume_bdk_out = round((volume_bdk_out - volume_strapping_floor), 2)
 
 
-    if (house_mode and (floor_seiling != 'Черновые'))  or (not house_mode and (floor_seiling != 'Чистовые')):
-
-
+    if floor_seiling != 'Базовый домокомплект':
 
         #for not default floor and seiling
 
@@ -150,12 +156,15 @@ def result(DATA, MODELS_PATH, slab_foundation, first_floor_area, house_mode, flo
         #0,025 - finishing ceiling thickness
         #coef_angle - coefficient of increase in the ceiling area of attic (1-1.3)
 
+        phrase[0] = phr4
+        phrase[1] = phr8
+
         coef_angle = 1
 
         if attic_type in ['Нет мансарды', 'Отсутствует'] or roof_type == 'Вальмовая':
             coef_angle = 1
         elif roof_type == 'Cложная':
-            coef_angle = 1,3
+            coef_angle = 1,25
         elif roof_type == 'Двускатная':
             if roof_slope_angle == 'Большой':
                 coef_angle = 1.3
@@ -167,55 +176,93 @@ def result(DATA, MODELS_PATH, slab_foundation, first_floor_area, house_mode, flo
             if roof_slope_angle == 'Большой':
                 coef_angle = 1.25
             elif roof_slope_angle == 'Средний':
-                    coef_angle = 1.2
+                    coef_angle = 1.15
             elif roof_slope_angle == 'Маленький':
-                    coef_angle = 1.1
+                    coef_angle = 1.05
 
 
         price_finish_ceiling_vl = 52500
         price_draft_ceiling_vl = 25600
+        price_finish_floor_vl = 48500
+        price_draft_floor_vl = 25600
 
 
         price_draft_floor_sq = price_draft_floor_vl * 1.04 * 0.03
-        price_finish_floor_sq = price_finishing_floor_vl * 1.04 * 0.04 * 1.07
-        price_draft_seiling_sq = price_draft_ceiling_vl * 0.03 * coef_angle
-        price_finish_floor_sq = price_draft_ceiling_vl * 0.025 * 1.07 * coef_angle
+        price_finish_floor_sq = price_finish_floor_vl * 1.04 * 0.04 * 1.07
+        price_draft_ceiling_sq = price_draft_ceiling_vl * 0.03 * coef_angle
+        price_finish_ceiling_sq = price_finish_ceiling_vl * 0.025 * 1.07 * coef_angle
 
         diff_floor = price_finish_floor_sq - price_draft_floor_sq
-        diff_ceiling = price_finish_floor_sq - price_draft_seiling_sq
+        diff_ceiling = price_finish_ceiling_sq - price_draft_ceiling_sq
 
         #attic area. = 0 if no attic
         attic_area = floor_area - first_floor_area
 
         if house_mode:
             if floor_seiling == 'Чистовые':
+                phrase[0] = phrase[0] + phr9
+                phrase[1] = phrase[1] + phr9
                 if not slab_foundation:
                     diff_fs = diff_floor + diff_ceiling
                     cost_bdk_out = round(cost_bdk_out + floor_area * diff_fs)
+                    volume_bdk_out = round(volume_bdk_out + floor_area * (0.014 - 0.003 * coef_angle), 2)
                 else:
+                    phrase[0] = phrase[1][:-2] + phr13
+                    phrase[0] = phrase[1][:-2] + phr13
                     cost_bdk_out = round(cost_bdk_out + floor_area * diff_ceiling + attic_area * diff_floor)
+                    volume_bdk_out = round(volume_bdk_out + attic_area * 0.014 - floor_area * 0.003 * coef_angle, 2)
             elif floor_seiling == 'Чистовой пол и черновой потолок':
+                phrase[0] = phrase[0] + phr11
+                phrase[1] = phrase[1] + phr11
                 if not slab_foundation:
                     cost_bdk_out = round(cost_bdk_out + floor_area * diff_floor)
+                    volume_bdk_out = round(volume_bdk_out + floor_area * 0.014, 2)
                 else:
+                    phrase[0] = phrase[0][:-2] + phr13
+                    phrase[1] = phrase[1][:-2] + phr13
                     cost_bdk_out = round(cost_bdk_out + attic_area * diff_floor)
+                    volume_bdk_out = round(volume_bdk_out + attic_area * 0.014, 2)
             elif floor_seiling == 'Чистовой потолок и черновой пол':
-                cost_bdk_out = round(cost_bdk_out + floor_area * diff_seling)
+                phrase[0] = phrase[0] + phr12
+                phrase[1] = phrase[1] + phr12
+                cost_bdk_out = round(cost_bdk_out + floor_area * diff_ceiling)
+                volume_bdk_out = round(volume_bdk_out - floor_area * 0.003 * coef_angle, 2)
+                if slab_foundation:
+                    phrase[1] = phrase[1][:-2] + phr13
+                    phrase[3] = phrase[3][:-2] + phr13
 
         if not house_mode:
             if floor_seiling == 'Черновые':
+                phrase[0] = phrase[0] + phr10
+                phrase[1] = phrase[1] + phr10
                 if not slab_foundation:
-                   diff_fs = diff_floor + diff_ceiling
-                   cost_bdk_out = round(cost_bdk_out - floor_area * diff_fs)
+                    diff_fs = diff_floor + diff_ceiling
+                    cost_bdk_out = round(cost_bdk_out - floor_area * diff_fs)
+                    volume_bdk_out = round(volume_bdk_out - floor_area * (0.014 - 0.003 * coef_angle), 2)
                 else:
-                   cost_bdk_out = round(cost_bdk_out - floor_area * diff_ceiling - attic_area * diff_floor)
+                    phrase[0] = phrase[0][:-2] + phr13
+                    phrase[1] = phrase[1][:-2] + phr13
+                    cost_bdk_out = round(cost_bdk_out - floor_area * diff_ceiling - attic_area * diff_floor)
+                    volume_bdk_out = round(volume_bdk_out - attic_area * 0.014 + floor_area * 0.003 * coef_angle, 2)
             elif floor_seiling == 'Чистовой пол и черновой потолок':
-                if not slab_foundation:
-                   cost_bdk_out = round(cost_bdk_out - floor_area * diff_floor)
-                else:
-                   cost_bdk_out = round(cost_bdk_out - attic_area * diff_floor)
+                phrase[0] = phrase[0] + phr11
+                phrase[1] = phrase[1] + phr11
+                cost_bdk_out  = round(cost_bdk_out - floor_area * diff_ceiling)
+                volume_bdk_out = round(volume_bdk_out + floor_area * 0.003 * coef_angle, 2)
+                if slab_foundation:
+                    phrase[0] = phrase[0][:-2] + phr13
+                    phrase[1] = phrase[1][:-2] + phr13
             elif floor_seiling == 'Чистовой потолок и черновой пол':
-               cost_bdk_out = round(cost_bdk_out - floor_area * diff_seling)
+                phrase[0] = phrase[0] + phr12
+                phrase[1] = phrase[1] + phr12
+                if not slab_foundation:
+                    cost_bdk_out = round(cost_bdk_out - floor_area * diff_floor)
+                    volume_bdk_out = round(volume_bdk_out - floor_area * 0.014, 2)
+                else:
+                    phrase[0] = phrase[0][:-2] + phr13
+                    phrase[1] = phrase[1][:-2] + phr13
+                    cost_bdk_out = round(cost_bdk_out - attic_area * diff_floor)
+                    volume_bdk_out = round(volume_bdk_out - attic_area * 0.014, 2)
 
 
     result_out(phrase, cost_bdk_out, volume_bdk_out, cost_sk_out, volume_sk_out)
@@ -230,27 +277,35 @@ def information_block_output (house_type, length, width, double_bar_per, attic_t
 
     if (house_type == 'Дом') and (double_bar_per == 0):
         house_type = 'Летний домик'
-
     st.write('')
-    st.write(house_type, ' размером ', str(round(length, 2)), 'x', str(round(width, 2)), ' м')
+    text1 = house_type + ' размером ' + str(round(length, 2)) + ' x ' + str(round(width, 2)) + ' м '
+    text2=''
     if (house_type == 'Баня') and (double_bar_per == 0):
-        st.write('из одинарного бруса')
+        text1 += 'из одинарного бруса'
     if double_bar_per == 100:
-        st.write('полностью из двойного бруса')
+        text1 += 'полностью из двойного бруса'
     elif double_bar_per == 60:
-        st.write('cо внешними стенами из двойного бруса, все внутренние перегородки из одинарного бруса')
+        text1 += 'cо внешними стенами из двойного бруса'
+        text2 = 'и всеми внутренними перегородками из одинарного бруса'
     elif double_bar_per in [65,70]:
-        st.write('со внешними стенами из двойного бруса, почти все внутренние перегородки из одинарного бруса')
+        text1 += 'со внешними стенами из двойного бруса'
+        text2 = 'и почти всеми внутренними перегородками из одинарного бруса'
     elif double_bar_per == 75:
-        st.write('со внешними стенами из двойного бруса, внутренние перегородки частично из двойного бруса')
+        text1 += 'со внешними стенами из двойного бруса'
+        text2 = 'и внутренними перегородками частично из двойного бруса'
     elif double_bar_per in [80,85]:
-        st.write('со внешними стенами из двойного бруса, большая часть внутренних перегородок из двойного бруса')
+        text1 += 'со внешними стенами из двойного бруса'
+        text2 = 'и большей частью внутренних перегородок из двойного бруса'
     elif double_bar_per in [90,95]:
-        st.write('со внешними стенами из двойного бруса, почти все внутренние перегородки из двойного бруса')
+        text1 += 'со внешними стенами из двойного бруса'
+        text2 = 'и почти всеми внутренними перегородками из двойного бруса'
     elif (double_bar_per > 0) and (double_bar_per < 50):
-        st.write('почти все стены и перегородки в нём(ней) из одинарного бруса')
+        text1 += 'почти все стены и перегородки в нём(ней) из одинарного бруса'
     else:
-        st.write('со стенами и перегородками большей частью из двойного бруса')
+        text1 += 'со стенами и перегородками большей частью из двойного бруса'
+    st.write(text1)
+    if text2:
+        st.write(text2)
     st.write('Мансарда', attic_type.lower())
     st.write('Площадь жилой зоны приблизительно ', str(floor_area), 'м.кв')
     if veranda_area == 0:
@@ -265,7 +320,7 @@ def information_block_output (house_type, length, width, double_bar_per, attic_t
         st.write('Пол и потолок черновые')
     elif floor_ceiling == 'Чистовые':
         st.write('Пол и потолок чистовые')
-    else:
+    elif floor_ceiling != 'Базовый домокомплект':
         st.write(floor_ceiling)
 
     return
@@ -308,6 +363,26 @@ def input_transform_data():
                             help='У поднятой мансарды высота боковых стен 1,56 метра')
     double_bar_per = st.slider('Процент двойного бруса', min_value=0, max_value=100, value=60, step=5,
                             help='Для одинарного бруса - 0, всё двойной брус - 100, все внешние стены двойные, все перегородки одинарные - 60')
+
+    if double_bar_per == 60:
+        st.write('Все внешние стены из двойного бруса, все перегородки - из одинарного')
+    elif double_bar_per == 0:
+        st.write('Полностью из одинарного бруса')
+    elif double_bar_per == 100:
+        st.write('Полностью из двойного бруса')
+    elif double_bar_per == 75:
+        st.write('Все внешние стены из двойного бруса, перегородки - пополам из двойного и одинарного')
+    elif 0 <= double_bar_per <= 45:
+        st.write('Из одинарного бруса с небольшими добавлениями двойного')
+    elif 50 <= double_bar_per <= 55:
+        st.write('Внешние стены из двойного и одинарного бруса, перегородки - из одинарного')
+    elif 65 <= double_bar_per <= 70:
+        st.write('Все внешние стены двойные, перегородки - в основном из одинарного')
+    elif 80 <= double_bar_per <= 85:
+        st.write('Все внешние стены двойные, перегородок - больше из двойного бруса')
+    elif 90 <= double_bar_per <= 95:
+        st.write('Все внешние стены и почти все перегородки из двойного бруса, очень малая часть перегородок - из одинарного')
+
     length = st.number_input('Длина в метрах', min_value=1.0, max_value=35.0, value=8.0, step=0.1,
                             help='По сторонам, на которые ложатся стропила и скаты крыши; вдоль обрешётки')
     width = st.number_input('Ширина в метрах', min_value=1.0, max_value=20.0, value=6.0, step=0.1,
@@ -362,12 +437,14 @@ def input_transform_data():
         attic_type = 'Отсутствует'
 
     #floors and ceilings (draft or finishin)
+
+    floor_ceiling = 'Базовый домокомплект'
     if house_type == 'Дом':
-        floor_ceiling = st.selectbox('Пол и потолок', ['Черновые', 'Чистовые', 'Чистовой пол и черновой потолок', 'Чистовой потолок и черновой пол'],
-                                      help='По умолчанию потолки и полы считаются черновыми', label_visibility="visible")
+        floor_ceiling = st.selectbox('Пол и потолок', ['Базовый домокомплект', 'Чистовые', 'Чистовой пол и черновой потолок', 'Чистовой потолок и черновой пол'],
+                                      help='По умолчанию для дома пол и потолок считаются черновыми', label_visibility="visible")
     else:
-        floor_ceiling = st.selectbox('Пол и потолок', ['Чистовые', 'Чистовой пол и черновой потолок', 'Чистовой потолок и черновой пол', 'Черновые'],
-                                      help='По умолчанию потолки и полы считаются чистовыми', label_visibility="visible")
+        floor_ceiling = st.selectbox('Пол и потолок', ['Базовый домокомплект', 'Чистовой пол и черновой потолок', 'Чистовой потолок и черновой пол', 'Черновые'],
+                                      help='По умолчанию для бани пол и потолок считаются чистовыми', label_visibility="visible")
 
 
     slab_foundation = st.checkbox('Плитный фундамент', value=False, help='Расчёт стоимостей под фундаментную плиту',
@@ -430,5 +507,5 @@ if start:
     # prediction of the cost of a basic house kit
     # as the average prediction of the two best neural network models
     # and CatBoost models
-    result(data, models_path, slab_foundation, first_floor_area, house_mode, floor_seiling, floor_area,
+    result(data, models_path, slab_foundation, first_floor_area, house_mode, floor_ceiling, floor_area,
            roof_type, roof_slope_angle, attic_type)
